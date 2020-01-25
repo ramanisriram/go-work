ee8bdf11a3e16686d647b0f6b26d0224b7dd565d
sriram1113 <sriram.sr.ramani@gmail.com>
2020-01-25 19:52:56 +1100

    added morestrings

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/sriram1113/go-work/hello/morestrings"
 )
 
 
+
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println(morestrings.ReverseRunes("Hello World"))
 }
 
--- hello/morestrings/go.mod
+++ hello/morestrings/go.mod
@@ -1,3 +1,5 @@
 module github.com/sriram1113/go-work/hello/morestrings
 
 go 1.13
+
+require github.com/sriram1113/go-work/hello v0.0.0-20200125082835-a53ac35b5dd9 // indirect
